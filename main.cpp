01bb01ee0823de430f35317ce2f9969472d2cfb4
Juan Angelo <111475642+JuanAngelo1@users.noreply.github.com>
2024-11-14 11:53:17 -0500

    Primer Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Autor  : Juan Angelo Flores Rubio
+ * Codigo : 20196493
+ * Created on 14 de noviembre de 2024, 12:32 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    cout<<"Algoritmo GA"<<endl;
+    
+    return 0;
+}
+
